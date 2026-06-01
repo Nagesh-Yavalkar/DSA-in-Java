aa94514f2b1a01bcb546bbfcc641c626ca8ca68a
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-06-01 18:13:46 +0530

    factors file updated

--- Lec11/factors.java
+++ Lec11/factors.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class factors {
+
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your Number :");
